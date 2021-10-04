995344432031c68f85235d4d0ecb74a3701d8e02
sarwarali847 <sarwarali847@gmail.com>
2021-10-04 11:22:28 +0530

    Added Lab Unit 5

--- Java_Lab-Assignment/src/Lab_Unit_5/Lab_5_2.java
+++ Java_Lab-Assignment/src/Lab_Unit_5/Lab_5_2.java
@@ -3,10 +3,29 @@ package Lab_Unit_5;
 public class Lab_5_2 {
 
 	public static void main(String[] args) {
-	
+	shape c=new circle();
+	c.draw();
+	shape s=new square();
+	s.draw();
 
 	}
 
+}
+
+abstract class shape{
+	abstract void draw();
+}
+class circle extends shape{
+	void draw() {
+		System.out.println("Drwaing circle....");
+	}
+	
+}
+
+class square extends shape{
+	void draw() {
+		System.out.println("Drwaing square...");
+	}
 }
 
 
